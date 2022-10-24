08c8de6e4f0549e8368659917d590695ce8da3b0
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-10-24 11:29:07 -0700

    Add pause update

--- groove/grooveApp.swift
+++ groove/grooveApp.swift
@@ -3,35 +3,67 @@ import DSFDockTile
 import MediaPlayer
 import SwiftUI
 
-@main
-struct grooveApp: App {
-  @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+class DockData: ObservableObject {
+  @Published var artist: String
+  @Published var album: String
+  @Published var song: String
+  @Published var artwork: NSImage?
+  @Published var playing: Bool
 
-  var body: some Scene {
-    WindowGroup {
-      ZStack {
-        ContentView()
-      }
-    }
+  init(artist: String, album: String, song: String, artwork: NSImage? = nil, playing: Bool) {
+    self.artist = artist
+    self.album = album
+    self.song = song
+    self.artwork = artwork
+    self.playing = playing
+  }
+
+  func update(other: DockData) {
+    self.artist = other.artist
+    self.album = other.album
+    self.song = other.song
+    self.artwork = other.artwork
+    self.playing = other.playing
+  }
+  
+  func isEqual(other: DockData) -> Bool {
+    let artist = self.artist == other.artist
+    let album = self.album == other.album
+    let song = self.song == other.song
+    let artwork = self.artwork?.isEqual(to: other.artwork) ?? true
+    let playing = self.playing == other.playing
+    return artist && album && song && artwork && playing
   }
 }
 
-struct DockData {
-  let artist: String
-  let album: String
-  let song: String
-  let artwork: NSImage
-}
-
 struct DockImage: View {
-  let data: DockData
+  @EnvironmentObject var data: DockData
 
   var body: some View {
     VStack {
       ZStack {
-        Image(nsImage: data.artwork).interpolation(.high).antialiased(true).resizable().cornerRadius(12).shadow(color: .black.opacity(0.36), radius: 1, x: 1, y: 2)
-        RoundedRectangle(cornerRadius: 12).fill(.black).opacity(0.3)
-        Text("\(data.song) - \(data.artist)").foregroundColor(.white).font(Font.body.weight(.semibold)).padding(4)
+        Image(nsImage: data.artwork!)
+          .interpolation(.high)
+          .antialiased(true)
+          .resizable()
+          .cornerRadius(16)
+          .shadow(color: .black.opacity(0.36), radius: 1, x: 1, y: 2)
+        RoundedRectangle(cornerRadius: 16)
+          .fill(.black)
+          .opacity(0.3)
+        VStack {
+          Text("\(data.song)").foregroundColor(.white).font(Font.body.weight(.semibold))
+          Text("\(data.artist)").foregroundColor(.white).font(Font.body.weight(.semibold))
+        }
+        .padding(4).opacity(data.playing ? 1 : 0)
+        Image(systemName: data.playing ? "pause.fill" : "play.fill")
+          .interpolation(.high)
+          .antialiased(true)
+          .resizable()
+          .shadow(color: .black.opacity(0.36), radius: 1, x: 1, y: 2)
+          .frame(width: 32, height: 32)
+          .foregroundColor(.white)
+          .opacity(!data.playing ? 0.85 : 0)
       }
     }.padding(12)
   }
@@ -39,11 +71,11 @@ struct DockImage: View {
 
 class DockViewController: NSViewController {
   var dockData: DockData?
-
   override func loadView() {
     view = NSView()
     if let data = dockData {
-      let view = NSHostingView(rootView: DockImage(data: data))
+      let content = DockImage().environmentObject(data)
+      let view = NSHostingView(rootView: content)
       view.frame = NSRect(x: 0, y: 0, width: 128, height: 128)
       self.view.addSubview(view)
     }
@@ -55,33 +87,63 @@ class DockViewController: NSViewController {
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-  var iTunesBridge: iTunesBridge
-
+  var iTunesBridge: iTunesBridge?
   var dockViewController = DockViewController()
+  var data = DockData(artist: "", album: "", song: "", artwork: nil, playing: false)
 
   lazy var updateDockTile: DSFDockTile.View = {
     dockViewController.loadView()
     return DSFDockTile.View(dockViewController)
   }()
 
-  override init() {
+  func loadApplescript() async -> iTunesBridge {
     Bundle.main.loadAppleScriptObjectiveCScripts()
     let iTunesBridgeClass: AnyClass = NSClassFromString("iTunesBridge")!
-    self.iTunesBridge = iTunesBridgeClass.alloc() as! iTunesBridge
-    super.init()
+    let iTunesBridge = iTunesBridgeClass.alloc() as! iTunesBridge
+    return iTunesBridge
   }
 
-  func applicationDidFinishLaunching(_: Notification) {
-    if let song = iTunesBridge.trackInfo {
+  func getDockData() -> DockData? {
+    guard let bridge = iTunesBridge else { return nil }
+    if let song = bridge.trackInfo {
       let artist = song["trackArtist"] as! String
       let album = song["trackAlbum"] as! String
       let name = song["trackName"] as! String
-      if let artwork = iTunesBridge.artwork {
-        dockViewController.update(DockData(artist: artist, album: album, song: name, artwork: artwork))
+      let playing = bridge.playerState == .playing
+      let artwork = bridge.artwork
+      return DockData(artist: artist, album: album, song: name, artwork: artwork, playing: playing)
+    }
+    return nil
+  }
+
+  func updateTile() {
+    if let newData = getDockData() {
+      self.data.update(other: newData)
+      self.dockViewController.update(self.data)
+      self.updateDockTile.display()
+    }
+  }
+
+  func applicationDidBecomeActive(_: Notification) {
+    // print("active")
+    // iTunesBridge?.playPause()
+  }
+
+  func applicationShouldHandleReopen(_: NSApplication, hasVisibleWindows _: Bool) -> Bool {
+    print("reopen")
+    self.iTunesBridge?.playPause()
+    self.updateTile()
+
+    return true
+  }
+
+  func applicationDidFinishLaunching(_: Notification) {
+    Task { [weak self] in
+      do {
+        self?.iTunesBridge = await self?.loadApplescript()
+        self?.updateTile()
       }
     }
-
-    updateDockTile.display()
   }
 
   func applicationWillTerminate(_: Notification) {}
