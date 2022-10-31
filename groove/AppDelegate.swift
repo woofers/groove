768bf0fbfcd62dfae0fa21ad4443051a2416dfd4
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-10-31 10:16:14 -0700

    Move to dock controller

--- groove/AppDelegate.swift
+++ groove/AppDelegate.swift
@@ -1,61 +1,22 @@
-import AppleScriptObjC
-import DSFDockTile
 import SwiftUI
 
-enum ClickType {
-  case normal, double, none
-}
-
 class AppDelegate: NSObject, NSApplicationDelegate {
-  var dockViewController = DockViewController()
-  var info: MusicInfo?
-  var lastClicked = ProcessInfo.processInfo.systemUptime
-  var lastClickType: ClickType = .none
-  var action: MusicInfo.Action = .none
-  
-  static let DOUBLE_CLICK = 0.3
-  static let IGNORE_CLICK = 0.09
-
-  lazy var updateDockTile: DSFDockTile.View = {
-    dockViewController.loadView()
-    return DSFDockTile.View(dockViewController)
-  }()
-
-  func updateTile() {
-    let data = self.info?.getData()
-    Task { [weak self] in
-      await self?.dockViewController.update(data)
-      DispatchQueue.main.async { [weak self] in
-        self?.updateDockTile.display()
-      }
-    }
-  }
+  var dockController: DockController?
 
   func applicationDidBecomeActive(_: Notification) {
 
   }
 
   func applicationShouldHandleReopen(_: NSApplication, hasVisibleWindows _: Bool) -> Bool {
-    let lastClick = self.lastClicked
-    let now = ProcessInfo.processInfo.systemUptime
-    let clickTime = now - lastClick
-    if clickTime > AppDelegate.IGNORE_CLICK {
-      if clickTime <= AppDelegate.DOUBLE_CLICK {
-        action = .skip
-        self.lastClickType = .double
-      } else {
-        action = .playPause
-        self.lastClickType = .normal
-      }
-      self.lastClicked = now
-    }
-    self.info?.perform(self.action)
+    self.dockController?.click()
     return true
   }
 
   func applicationDidFinishLaunching(_: Notification) {
-    self.info = MusicInfo(self.updateTile)
+    self.dockController = DockController()
   }
 
-  func applicationWillTerminate(_: Notification) {}
+  func applicationWillTerminate(_: Notification) {
+    self.dockController?.destroy()
+  }
 }
--- groove/DockController.swift
+++ groove/DockController.swift
@@ -1,10 +1,19 @@
 import DSFDockTile
 import SwiftUI
 
+enum ClickType {
+  case normal, double, none
+}
+
 class DockController {
   var dockViewController = DockViewController()
   var info: MusicInfo?
-
+  var lastClicked = ProcessInfo.processInfo.systemUptime
+  var lastClickType: ClickType = .none
+  
+  static let DOUBLE_CLICK = 0.3
+  static let IGNORE_CLICK = 0.09
+  
   lazy var updateDockTile: DSFDockTile.View = {
     dockViewController.loadView()
     return DSFDockTile.View(dockViewController)
@@ -14,18 +23,36 @@ class DockController {
     self.info = MusicInfo(self.updateTile)
     self.updateTile()
   }
+  
+  func destroy() {
+    self.info?.destroy()
+  }
 
   func updateTile() {
-    /*
-    self.dockViewController.update(self.info?.fetch())
-    DispatchQueue.main.async { [weak self] in
-      self?.updateDockTile.display()
+    let data = self.info?.getData()
+    Task { [weak self] in
+      await self?.dockViewController.update(data)
+      DispatchQueue.main.async { [weak self] in
+        self?.updateDockTile.display()
+      }
     }
-     */
   }
 
   func click() {
-    self.info?.playPause()
-    self.updateTile()
+    let lastClick = self.lastClicked
+    let now = ProcessInfo.processInfo.systemUptime
+    let clickTime = now - lastClick
+    var action: MusicInfo.Action = .none
+    if clickTime > DockController.IGNORE_CLICK {
+      if clickTime <= DockController.DOUBLE_CLICK {
+        action = .skip
+        self.lastClickType = .double
+      } else {
+        action = .playPause
+        self.lastClickType = .normal
+      }
+      self.lastClicked = now
+    }
+    self.info?.perform(action)
   }
 }
